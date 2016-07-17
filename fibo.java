0a4cfeb417742b476cca88396cd4196848a8ab6e
sanghaviravindran <sangu.abirami@gmail.com>
2016-07-17 19:14:35 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,10 @@
+ int num=5,i,f1=0,f2=1,fibo;
+        System.out.print(f1+"\t"+f2);
+        for(i=2;i<5;i++)
+        {
+            fibo=f1+f2;
+            System.out.print("\t"+fibo);
+            f1=f2;
+            f2=f3;
+        }
+       
